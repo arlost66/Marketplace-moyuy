340adf7e2ca16929862c299221605de7a5e86390
John Patrick <jpatrickzephyr@gmail.com>
2022-10-05 23:36:43 +0800

    feat(add): add cloudinary implmentations

--- routes/admin.js
+++ routes/admin.js
@@ -1,52 +1,53 @@
 const express = require('express');
 const adminRouter = express.Router();
+const { upload } = require('../utils/cloudinary');
 //const passport = require('passport');
 
-const { adminHomepage,
-    isAdmin,
+const {
+  adminHomepage,
+  isAdmin,
 
+  getCustomerManagement,
+  editCustomerManagement,
 
-    getCustomerManagement,
-    editCustomerManagement,
-
-    getProductManagement,
-    addProductManagement,
-    editProductManagement,
-    deleteProductManagement
+  getProductManagement,
+  addProductManagement,
+  editProductManagement,
+  deleteProductManagement,
 } = require('../controllers/admin');
 
-const { checkAuthenticated,
-    checkNotAuthenticated
+const {
+  checkAuthenticated,
+  checkNotAuthenticated,
 } = require('../controllers/users');
-
 
 //prefix admin
 
+adminRouter
+  .route('/') //homepage sa admin
+  .get(checkAuthenticated, isAdmin, adminHomepage); //admin Dashboard Route
 
-adminRouter.route('/')//homepage sa admin
-    .get(checkAuthenticated, isAdmin, adminHomepage);//admin Dashboard Route
-
-
-adminRouter.route('/customer-management')
-    .get(checkAuthenticated, isAdmin, getCustomerManagement)
-    //  .post(checkAuthenticated, isAdmin, addCustomerManagement)
-    .put(checkAuthenticated, isAdmin, editCustomerManagement)
+adminRouter
+  .route('/customer-management')
+  .get(checkAuthenticated, isAdmin, getCustomerManagement)
+  //  .post(checkAuthenticated, isAdmin, addCustomerManagement)
+  .put(checkAuthenticated, isAdmin, editCustomerManagement);
 // .delete(checkAuthenticated, isAdmin, deleteCustomerManagement)
 
 //product management routers
-adminRouter.route('/product-management')
-    .get(checkAuthenticated, isAdmin, getProductManagement)
-    .post(checkAuthenticated, isAdmin, addProductManagement)
-    .put(checkAuthenticated, isAdmin, editProductManagement)
-    .delete(checkAuthenticated, isAdmin, deleteProductManagement)
-
-
+adminRouter
+  .route('/product-management')
+  .get(checkAuthenticated, isAdmin, getProductManagement)
+  .post(
+    checkAuthenticated,
+    isAdmin,
+    upload.single('image'),
+    addProductManagement
+  )
+  .put(checkAuthenticated, isAdmin, editProductManagement)
+  .delete(checkAuthenticated, isAdmin, deleteProductManagement);
 
 /*adminRouter.route('/stock-management')
     .get(checkAuthenticated, isAdmin, checkNotAuthenticatedAdmin, getStockManagement)*/
-
-
-
-
 
 module.exports = adminRouter;
---
+++ utils/cloudinary.js
@@ -0,0 +1,22 @@
+const multer = require('multer');
+
+const cloudinary = require('cloudinary').v2;
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'samples',
+  },
+});
+const upload = multer({
+  storage: storage,
+});
+
+module.exports = { upload };
